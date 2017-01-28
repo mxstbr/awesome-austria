89dc4c0de4b6b604383b314d67c0e3e73af30ffc
Max <contact@mstoiber.com>
2017-01-28 20:01:51 +0100

    Improve Danger output

--- dangerfile.js
+++ dangerfile.js
@@ -1,15 +1,20 @@
-import { danger, fail } from 'danger'
+import { danger, fail, markdown } from 'danger'
 
-const hasGoodTitle = danger.github.pr.title.toLowerCase().indexOf('update readme') === -1
+const TITLE_CHARACTER_MINIMUM = 5
+
+let text = ''
+const hasGoodTitle = danger.github.pr.title.toLowerCase().indexOf('update readme') === -1 && danger.github.pr.title.length > TITLE_CHARACTER_MINIMUM
 
 if (!hasGoodTitle) {
-	fail('Please read the contribution guidelines and give your PR a proper title explaining what you added.')
-}
-
-const changedList = danger.git.modified_files.includes('README.md')
-if (changedList) {
+	text += 'didn\'t follow the [contribution guidelines](https://github.com/mxstbr/awesome-austria/blob/master/contributing.md). Please give your PR a proper title explaining what you changed!'
+} else {
 	const readContributionGuidelines = danger.github.pr.body.indexOf('<!-- Leave this text in and start your description of the change below -->') > -1
 	if (!readContributionGuidelines) {
-		fail('Please read the contribution guidelines.')
+		text += 'didn\'t follow the [contribution guidelines](https://github.com/mxstbr/awesome-austria/blob/master/contributing.md).'
 	}
+}
+
+if (text.length > 0) {
+	fail('Oops, something went wrong...')
+	markdown(`\nThanks for the pull request, @${danger.github.pr.user.login}! I took a look to make sure it's ready for merging, and noticed you ${text}\n\nCan you please update the PR to address this?`)
 }
